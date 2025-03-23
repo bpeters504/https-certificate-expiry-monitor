4b7d4b3a80191de5c3ca2e1704c68704561cc8d2
Bill Peters <bill.peters@gmail.com>
2025-03-23 13:23:26 -0400

    changed the prompt to make it more obvious I'm in nix-shell

--- shell.nix
+++ shell.nix
@@ -6,4 +6,9 @@ mkShell {
     python312Packages.waitress
     python312Packages.configparser
   ];
+
+  shellHook = ''
+    export PS1='\n\[\033[1;91m\][nix-shell:\[\033[1;32m\]\w]\$\[\033[0m\] '
+  '';
+
 }
